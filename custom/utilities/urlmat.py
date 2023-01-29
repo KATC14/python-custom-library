3d51c3bc70bdaae47ede76d99c26ed3faa31a078
KATC14 <31497908+KATC14@users.noreply.github.com>
2023-01-29 17:44:37 -0500

    Delete urlmat.py

--- custom/utilities/urlmat.py
+++
@@ -1,7 +0,0 @@
-import urllib.parse
-
-def url_format(url:str, path:str) -> str:
-	if   path == 'encode':
-		return urllib.parse.quote(url)
-	elif path == 'decode':
-		return urllib.parse.unquote(url)
