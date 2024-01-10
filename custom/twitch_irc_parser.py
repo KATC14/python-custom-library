c792687e380333fb08ea1a6aaf0a3354efc96394
KATC14 <31497908+KATC14@users.noreply.github.com>
2024-01-10 01:52:06 -0500

    Update twitch_irc_parser.py
    updated twitch irc tags
    changed to not have nested dicts outside of badges

--- custom/twitch_irc_parser.py
+++ custom/twitch_irc_parser.py
@@ -1,76 +1,102 @@
+# modification of https://github.com/KATC14/twitch_irc_parser
+
 import traceback
 
-from schema import Schema, Use, Optional, Or
+from schema import Optional, Or, Schema, Use
 
 schema = Schema({
 	"command": str,
 	"channel": Or(str, None),
-    "tags": Or({
-        Optional("badge-info"):                Or(str, None, any),
-		Optional("badges"):                    Or({
-			Optional("moderator"):  Or(Use(int), str),
-			Optional("subscriber"): Or(Use(int), str),
-			Optional("premium"):    Or(Use(int), str),
-			Optional("turbo"):      Or(Use(int), str),
-			Optional(str):          Or(Use(int), str)
-		}, dict, None),
-        Optional("client-nonce"):              str,
-        Optional("color"):                     Or(str, None),
-        Optional("display-name"):              str,
-        Optional("emote-only"):                Or(Use(int), str, None),
-        Optional("followers-only"):            Or(Use(int), str, None),
-        Optional("emotes"):                    Or(None, str, dict),
-        Optional("emote-sets"):                list,
-        Optional("first-msg"):                 Or(Use(int), str),
-        Optional("flags"):                     Or(str, None),
-        Optional("id"):                        str,
-        Optional("mod"):                       Or(Use(int), str),
-        Optional("reply-parent-display-name"): str,
-        Optional("reply-parent-msg-body"):     str,
-        Optional("reply-parent-msg-id"):       str,
-        Optional("reply-parent-user-id"):      Or(Use(int), str),
-        Optional("reply-parent-user-login"):   str,
-        Optional("target-msg-id"):             str,
-        Optional("returning-chatter"):         Or(Use(int), str),
-        Optional("room-id"):                   Or(Use(int), str, None),
-        Optional("subscriber"):                Or(Use(int), str),
-        Optional("tmi-sent-ts"):               Or(Use(int), str),
-        Optional("turbo"):                     Or(Use(int), str),
-        Optional("login"):                     str,
-        Optional("msg-id"):                    str,
-        Optional("slow"):                      Or(Use(int), str),
-        Optional("system-msg"):                Or(str, None),
-        Optional("user-id"):                   Or(Use(int), str),
-        Optional("user-type"):                 Or(str, None)
-    }, None),
-    "source": Or({
-        Optional("nick"): Or(str, None),
-        Optional("host"): str
-    }, None),
-    #"command": {
-		#"command":                       str,
-        #Optional("isCapRequestEnabled"): bool,
-        #Optional("botCommand"):          Or(str, None),
-        #Optional("botCommandParams"):    Or(str, None)
-    #},
-    "message": Or(str, None)
+	Optional("message"): Or(str, None),
+	"host": Or(str, None),
+	Optional("nick"): Or(str, None),
+	Optional("badge-info"):                         Or(str, None, any),
+	Optional("badges"):                             Or({
+		Optional("moderator"):  Or(Use(int), str),
+		Optional("subscriber"): Or(Use(int), str),
+		Optional("premium"):    Or(Use(int), str),
+		Optional("turbo"):      Or(Use(int), str),
+		Optional(str):          Or(Use(int), str)
+	}, dict, None),
+	Optional("client-nonce"):                       str,
+	Optional("color"):                              Or(str, None),
+	Optional("display-name"):                       str,
+	Optional("emote-only"):                         Or(Use(int), str, None),
+	Optional("followers-only"):                     Or(Use(int), str, None),
+	Optional("emotes"):                             Or(None, str, dict),
+	Optional("emote-sets"):                         list,
+	Optional("first-msg"):                          Or(Use(int), str),
+	Optional("flags"):                              Or(str, None),
+	Optional("id"):                                 str,
+	Optional("bits"):                               Or(Use(int), str),
+	Optional("mod"):                                Or(Use(int), str),
+	Optional("vip"):                                Or(Use(int), str),
+	Optional("reply-parent-display-name"):          str,
+	Optional("reply-parent-msg-body"):              str,
+	Optional("reply-parent-msg-id"):                str,
+	Optional("reply-parent-user-id"):               Or(Use(int), str),
+	Optional("reply-parent-user-login"):            str,
+	Optional("reply-thread-parent-msg-id"):         Or(Use(int), str),
+	Optional("pinned-chat-paid-amount"):            Or(Use(int), str),
+	Optional("pinned-chat-paid-currency"):          str,
+	Optional("pinned-chat-paid-exponent"):          Or(Use(float), int, str),
+	Optional("pinned-chat-paid-level"):             str,
+	Optional("pinned-chat-paid-is-system-message"): Or(Use(bool), int, str),
+	Optional("msg-param-was-gifted"):               Use(bool),
+	Optional("msg-param-cumulative-months"):        Use(int),
+	Optional("msg-param-displayName"):              str,
+	Optional("msg-param-login"):                    str,
+	Optional("msg-param-months"):                   Use(int),
+	Optional("msg-param-multimonth-duration"):      Use(int),
+	Optional("msg-param-multimonth-tenure"):        Use(int),
+	Optional("msg-param-promo-gift-total"):         Or(Use(int), str),
+	Optional("msg-param-promo-name"):               str,
+	Optional("msg-param-recipient-display-name"):   str,
+	Optional("msg-param-recipient-id"):             str,
+	Optional("msg-param-recipient-user-name"):      str,
+	Optional("msg-param-sender-login"):             str,
+	Optional("msg-param-sender-name"):              str,
+	Optional("msg-param-should-share-streak"):      Use(int),
+	Optional("msg-param-streak-months"):            Or(Use(int), str),
+	Optional("msg-param-mass-gift-count"):          Or(Use(int), str),
+	Optional("msg-param-sender-count"):             Or(Use(int), str),
+	Optional("msg-param-sub-plan"):                 Or(Use(int), str),
+	Optional("msg-param-sub-plan-name"):            str,
+	Optional("msg-param-viewerCount"):              Or(Use(int), str),
+	Optional("msg-param-ritual-name"):              str,
+	Optional("msg-param-threshold"):                Or(Use(int), str),
+	Optional("msg-param-origin-id"):                Or(Use(int), str),
+	Optional("msg-param-community-gift-id"):        Or(Use(int), str),
+	Optional("msg-param-gift-months"):              Or(Use(int), str),
+	Optional("msg-param-fun-string"):               str,
+	Optional("msg-param-gift-theme"):               str,
+	Optional("target-msg-id"):                      str,
+	Optional("returning-chatter"):                  Or(Use(int), str),
+	Optional("room-id"):                            Or(Use(int), str, None),
+	Optional("subscriber"):                         Or(Use(int), str),
+	Optional("tmi-sent-ts"):                        Or(Use(int), str),
+	Optional("turbo"):                              Or(Use(int), str),
+	Optional("login"):                              str,
+	Optional("msg-id"):                             str,
+	Optional("slow"):                               Or(Use(int), str),
+	Optional("system-msg"):                         Or(str, None),
+	Optional("user-id"):                            Or(Use(int), str),
+	Optional("user-type"):                          Or(str, None)
 })
 
-# Parses an IRC message and returns a JSON object with the message's 
-# component parts (command, channel, badges, tags, source (nick and host), message). 
-# Expects function receive a single message at a time. (Twitch IRC server may 
+# Parses an IRC message and returns a JSON object with the message's
+# component parts (command, channel, badges, tags, source (nick and host), message).
+# Expects function receive a single message at a time. (Twitch IRC server may
 # send one or more IRC messages in a single message.)
 def msg_parser(message):
 	parsed_message = {# Contains the component parts.
 		"command": None,
 		"channel": None,
-		"tags": None,
-		"source": None,
-		"message": None
+		"host": None
 	}
 
 	# The start index. Increments as we parse the IRC message.
-	idx = 0 
+	idx = 0
 
 	# The raw components of the IRC message.
 	tags_component = source_component = command_component = parameters_component = None
@@ -102,7 +128,7 @@ def msg_parser(message):
 		parameters_component = message[idx:]
 
 	# Parse the command component of the IRC message.
-	parsed_message.update({i: x for i, x in parse_command(command_component).items()})
+	parsed_message.update(parse_command(command_component))
 
 	# Only parse the rest of the components if it's a command
 	# we care about we ignore some messages.
@@ -112,13 +138,15 @@ def msg_parser(message):
 			#if pt.get('badges'):
 			#	parsed_message['badges'] = pt['badges']
 			#	del pt['badges']
-			parsed_message['tags'] = pt
+			parsed_message.update(pt)
 
-		parsed_message['source'] = parseSource(source_component)
+		if source_component:# Not all messages contain a source
+			parsed_message.update(parseSource(source_component))
 
-		parsed_message['message'] = parameters_component if parameters_component != '-' else None
-		#if rawParametersComponent and rawParametersComponent[0] == '!':  
-		#	# The user entered a bot command in the chat window.            
+		if parameters_component != '-':
+			parsed_message['message'] = parameters_component
+		#if rawParametersComponent and rawParametersComponent[0] == '!':
+		#	# The user entered a bot command in the chat window.
 		#	parsed_message['command'] = parseParameters(rawParametersComponent, parsedMessage['command'])
 
 	try:
@@ -164,15 +192,17 @@ def parseTags(tags):
 							position_parts = position.split('-')
 							text_positions.append({
 								"start_position": position_parts[0],
-								"end_position": position_parts[1]    
+								"end_position": position_parts[1]
 							})
 
 						emotes_dict[emote_parts[0]] = text_positions
 					paresed_tags[split_tags[0]] = emotes_dict
 				else:
 					paresed_tags[split_tags[0]] = None
-			case 'reply-parent-msg-body':
-				paresed_tags[split_tags[0]] = value.replace('\\s', ' ')
+			case 'system-msg' | 'msg-param-sub-plan-name' | 'reply-parent-msg-body':
+				paresed_tags[split_tags[0]] = value.replace('\\s', ' ') if value else value
+			case 'msg-param-was-gifted':
+				paresed_tags[split_tags[0]] = True if value == 'true' else False
 			case 'emote-sets':
 				# emote-sets=0,33,50,237
 				# Array of emote set IDs.
@@ -181,13 +211,14 @@ def parseTags(tags):
 				# If the tag is in the list of tags to ignore, ignore
 				# it otherwise, add it.
 				#if not tagsToIgnore.get(parsedTag[0]):
+				#print(f"{split_tags[0]} -> {value} <-")
 				paresed_tags[split_tags[0]] = value
 
 	return paresed_tags
 
 # Parses the command component of the IRC message.
 def parse_command(command_component):
-	parsed_command = None
+	parsed_command = {}
 	command_parts = command_component.split(' ')
 
 	match command_parts[0]:
@@ -216,8 +247,8 @@ def parse_command(command_component):
 				"command": command_parts[0],
 				"channel": command_parts[1][1:]
 			}
-		case 'RECONNECT':  
-			print('The Twitch IRC server is about to terminate the connection for maintenance.')
+		case 'RECONNECT':
+			#print('The Twitch IRC server is about to terminate the connection for maintenance.')
 			parsed_command = {
 				"command": command_parts[0]
 			}
@@ -225,9 +256,9 @@ def parse_command(command_component):
 			parsed_command = {
 				"command": command_parts[0]
 			}
-			print(f'Unsupported IRC command: {command_parts[2]}')
+			print(f'[parser] Unsupported IRC command: {command_parts[2]}')
 		# 001 Logged in (successfully authenticated).
-		
+
 		# 353 Tells you who else is in the chat room you're joining.
 		case '001' | '002' | '003' | '004' | '353' | '366' | '372' | '375' | '376':
 			parsed_command = {
@@ -242,25 +273,21 @@ def parse_command(command_component):
 		#	}
 		#	#print(f'numeric message: {commandParts[0]}')
 		case _:
-			print(f'Unexpected command: {command_parts[0]}')
+			print(f'[parser] Unexpected command: {command_parts[0]}')
 
 	return parsed_command
 
 # Parses the source (nick and host) components of the IRC message.
 def parseSource(source_component):
-	if not source_component:# Not all messages contain a source
-		return None
-	else:
-		source_parts = source_component.split('!')
-		return {
-			"nick": source_parts[0] if len(source_parts) == 2 else None,
-			"host": source_parts[1] if len(source_parts) == 2 else source_parts[0]
-		}
+	source_parts = source_component.split('!')
+	parsed = {"host": source_parts[1] if len(source_parts) == 2 else source_parts[0]}
+	if len(source_parts) == 2: parsed['nick'] = source_parts[0]
+	return parsed
 
 # Parsing the IRC parameters component if it contains a command (e.g., !dice).
 #def parseParameters(parameters_component, command):
 #	idx = 0
-#	command_parts = parameters_component[idx + 1:].strip() 
+#	command_parts = parameters_component[idx + 1:].strip()
 #	params_Idx = command_parts.find(' ')
 #	print(command_parts, params_Idx)
 #
